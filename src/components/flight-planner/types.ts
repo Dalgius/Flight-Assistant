b8782a0fdd0f8279b674d28cb2c5196dab560507
Dalgius <134711586+Dalgius@users.noreply.github.com>
2025-06-27 20:10:44 +0200

    Delete src directory

--- src/components/flight-planner/types.ts
+++
@@ -1,7 +0,0 @@
-export type PanelType = 'settings' | 'waypoints' | 'pois' | 'missions' | 'terrain' | 'file' | 'stats';
-
-export type DialogType = 'orbit' | 'survey' | 'facade' | null;
-
-export interface PanelProps {
-  onOpenDialog: (dialog: DialogType) => void;
-}
